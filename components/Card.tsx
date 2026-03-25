5d082eb30dc49a617f74853e2f84f71655a01e97
AndreiAndrewDrew <andreiandrewg@gmail.com>
2026-03-25 11:17:33 +0200

    ClouDrew : Optimization

--- components/Card.tsx
+++ components/Card.tsx
@@ -2,9 +2,8 @@
 import Link from 'next/link';
 import { Models } from 'node-appwrite';
 import Thumbnail from './Thumbnail';
-import { convertFileSize } from '@/lib/utils';
-// import FormattedDateTime from './FormattedDateTime';
 import ActionDropdown from './ActionDropdown';
+import { convertFileSize } from '@/lib/utils';
 
 const Card = ({
   file,
@@ -15,27 +14,21 @@ const Card = ({
 }) => {
   return (
     <Link href={(file as any).url} target="_blank" className="file-card">
-      <div className="flex justify-between">
+      <div className="flex justify-between gap-3">
         <Thumbnail
           type={(file as any).type}
           extension={(file as any).extension}
           url={(file as any).url}
-          className="size-18"
-          // imageClassName="size-11!"
+          className="size-12"
         />
 
-        <div className="flex flex-col items-end justify-between">
+        <div className="recent-file-details">
+          <div className="flex flex-col gap-1">
+            <p className="recent-file-name">{(file as any).name}</p>
+            <p className="body-2">{convertFileSize((file as any).size)}</p>
+          </div>
           <ActionDropdown file={file} owner={owner} />
-          <p className="body-2">{convertFileSize((file as any).size)}</p>
         </div>
-      </div>
-      <div className="file-card-details">
-        <p className="subtitle-2 line-clamp-1">{(file as any).name}</p>
-        {/* <FormattedDateTime
-          date={file.$createdAt}
-          className="body-2 text-light-100"
-        />
-        <p className="caption line-clamp-1 text-light-200">By: {owner}</p> */}
       </div>
     </Link>
   );
